5928c6ad35a8dc1e2e5acb848993233b224ee15c
Ernest-Gray <egray4cs@gmail.com>
2022-03-21 16:59:52 -0400

    More Updates

--- Scripts/IdleVoiceActionPlayer.cs
+++ Scripts/IdleVoiceActionPlayer.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class IdleVoiceActionPlayer : MonoBehaviour
 {
+    public VoiceActionSet voiceActionSet;
+    public AudioSource audioSource;
+
+    [Min(0f)]
+    public float minDelay = 10;
+    public float maxDelay = 20;
+
+    [Tooltip("This must be the same as the voice action name you want to play")]
+    public string idleActionString = "idle";
+
+    private float TimeTilNextSound;
+
+    private void OnValidate()
+    {
+        if (minDelay > maxDelay)
+        {
+            minDelay = maxDelay;
+        }
+        if (maxDelay < minDelay)
+        {
+            maxDelay = minDelay;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeTilNextSound = Time.time + Random.Range(minDelay, maxDelay);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > TimeTilNextSound)
+        {
+            voiceActionSet.PlayAction(idleActionString, audioSource);
+            TimeTilNextSound = Time.time + Random.Range(minDelay, maxDelay) + audioSource.clip.length;
+        }
     }
 }
